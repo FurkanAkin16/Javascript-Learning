e22fc8cfbb60ab21985065b8b599c3853a7fd576
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2023-03-20 13:11:10 +0300

    Add files via upload

---
+++ Fundamentals-Part-1-JS/script.js
@@ -0,0 +1,31 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Major');
+console.log('11');
+
+let firstName = "Major";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda ="JM";
+let $function = 27;
+
+let person = "Major";
+let PI =3.1415;
+
+let myFirstJob ='Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+
+console.log(myFirstJob);
+
+let isIsland = 'Turkey is in Europe ';
+let isIslandLanguage = "and its 85 million people speak turkish language";
+
+console.log (isIsland);
+console.log (isIslandLanguage);
