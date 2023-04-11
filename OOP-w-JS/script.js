037bd638d4fc896609b04dc61d295b6d28b8de32
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2023-04-12 00:12:34 +0300

    Add files via upload

--- OOP-w-JS/script.js
+++ OOP-w-JS/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
+/*const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
   // Never to this
   /* this.calcAge = function () {
     console.log(2037 - this.birthYear);
-  };*/
+  };
 };
 
 const major = new Person('Major', 1998);
@@ -53,7 +53,7 @@ Array.prototype.unique = function () {
 console.log(arr.unique());
 
 // Coding Challange #1
-const Car = function (name, speed) {
+/*const Car = function (name, speed) {
   this.name = name;
   this.speed = speed;
 };
@@ -84,4 +84,110 @@ bmw.brake();
 bmw.accelerate();
 bmw.accelerate();
 bmw.accelerate();
-mercedes.brake();
+mercedes.brake(); */
+
+// Class expression
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+}
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+/*PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};*/
+jessica.greet();
+const walter = new PersonCl('Walter White', 1985);
+
+// 1. Classes are NOT hoiseted
+// 2. Classes are first-class citizes
+// 3. Classes are executed in strict mode
+
+// Coding Challange 2
+
+class CarCl {
+  constructor(name, speed) {
+    this.name = name;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.name} is going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.name} is going at ${this.speed} km/h`);
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
+/*const account = {
+  owner: 'Major',
+  movements: [200, 530, 120, 300],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();*/
