de640653ade16290361513af892041d2b8476973
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2023-04-24 16:04:01 +0300

    Add files via upload

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -118,7 +118,7 @@ setTimeout(() => {
 };
 getCountryData('turkey');*/
 
-const getCountryData = function (country) {
+/*const getCountryData = function (country) {
   // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
@@ -141,5 +141,39 @@ const getCountryData = function (country) {
 };
 
 btn.addEventListener('click', function () {
-  getCountryData('asdasd');
-});
+  getCountryData('turkey');
+});*/
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmI = async function () {
+  try {
+    //Geolocation
+    const pos = await getPosition();
+
+    const { latitude: lat, longitude: lng } = pos.coords;
+    //Reverse geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!resGeo.ok) throw new Error('Problem getting location data');
+
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
+    // Country data
+    const res = await fetch(
+      `https://restcountries.com/v2/name/${dataGeo.country}`
+    );
+    if (!res.ok) throw new Error('Problem getting country');
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data[0]);
+  } catch (err) {
+    console.error(`${err}`);
+    renderError(` 💥 ${err.message}`);
+  }
+};
+whereAmI();
+console.log('FIRST');
