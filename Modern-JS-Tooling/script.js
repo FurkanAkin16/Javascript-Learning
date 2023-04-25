4204a9b5774cc2e8853d1f1aea365f3c8f20c6fc
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2023-04-25 14:42:05 +0300

    Add files via upload

---
+++ Modern-JS-Tooling/script.js
@@ -0,0 +1,60 @@
+// Importing Module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+//addToCart('bread', 5);
+// console.log(price, tq);
+// console.log('Importing Module');
+//import * as ShoppingCard from './shoppingCart.js';
+//ShoppingCard.addToCart('bread', 5);
+
+//import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+//console.log(price);
+import shoppingCart from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 5);
+// add('apples', 4);
+
+// console.log(cart);
+
+// // console.log('Start fetching');
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
+// // console.log('Something');
+
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+
+// const lastPost = getLastPost();
+// console.log(lastPost);
+// // Not very clean
+// // lastPost.then(last => console.log(last));
+
+// const lasPost2 = await getLastPost();
+// console.log(lasPost2);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product}  ordered from supplier`);
+  };
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
