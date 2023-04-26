c491481a1e2a639ae6676dec79971fb0bc892148
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2023-04-26 13:23:03 +0300

    Add files via upload

--- Modern-JS-Tooling/script.js
+++ Modern-JS-Tooling/script.js
@@ -8,13 +8,13 @@
 
 //import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 //console.log(price);
-import shoppingCart from './shoppingCart.js';
+//import shoppingCart from './shoppingCart.js';
 import add, { cart } from './shoppingCart.js';
-// add('pizza', 2);
-// add('bread', 5);
-// add('apples', 4);
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
 
-// console.log(cart);
+console.log(cart);
 
 // // console.log('Start fetching');
 // // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -37,24 +37,63 @@ import add, { cart } from './shoppingCart.js';
 // const lasPost2 = await getLastPost();
 // console.log(lasPost2);
 
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`);
-  };
-  const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product}  ordered from supplier`);
-  };
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('pizza', 2);
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product}  ordered from supplier`);
+//   };
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+
+const major = new Person('Major');
+console.log('Major' ?? null);
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+
+// import 'core-js/stable/array/find.js';
+
+import 'regenerator-runtime/runtime.js';
